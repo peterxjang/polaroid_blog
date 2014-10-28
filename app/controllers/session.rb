c17eec3d2997756ed8e553d556a092f7916e0fe0
Peter Jang <peter.x.jang@gmail.com>
2014-10-27 20:49:45 -0500

    added create user to homepage

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -13,10 +13,22 @@ post '/sessions' do
     session[:user_id] = user.id
     redirect '/posts'
   else
-    @message = "Incorrect email or password!"
+    @message_signin = "Incorrect email or password!"
     erb :'sessions/new'
   end
 end
+
+post '/users' do
+  user = User.new(username: params[:username], password: params[:password])
+  if user.valid?
+    session[:user_id] = user.id
+    redirect '/posts'
+  else
+    @message_signup = "Invalid email or password!"
+    erb :'sessions/new'
+  end
+end
+
 
 get '/signout' do
   session.delete(:user_id)
